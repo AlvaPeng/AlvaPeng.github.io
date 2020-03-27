f4d93c1eaad078c9735c33fbd2feacc09acd07b1
alva_peng <alvapeng97@163.com>
2020-03-27 18:56:17 +0800

    update

--- js/main.js
+++ js/main.js
@@ -5,3 +5,48 @@ $(document).ready(function(){
     $("#panel").slideToggle(280, 'swing');
   });
 });
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById('draw-2d');
+const context = myPics.getContext('2d');
+
+// The x and y offset of the canvas from the edge of the page
+const rect = myPics.getBoundingClientRect();
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+myPics.addEventListener('mousedown', e => {
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  isDrawing = true;
+});
+
+myPics.addEventListener('mousemove', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'white';
+  context.lineWidth = 2;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+  console.log("drawing");
+}
