7c8d18fc93fe8d36879b425000fc03a47003573a
Alva Peng <alvapeng97@163.com>
2022-05-29 15:18:32 +0800

    delete!

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#flip")
-    .css('cursor', 'pointer')
-    .click(function(){
-    $("#panel").slideToggle(280, 'swing');
-  });
-});
--- js/music-test.js
+++
@@ -1,7 +0,0 @@
-const synth = new Tone.Synth().toMaster();
-synth.triggerAttackRelease("C4","8n"); // key + duration(这里是八分音符)
-
-// 没有用户行为 web 不会自动播放音频
-document.querySelector('button').addEventListener('click', async () => {
-	await Tone.start()
-})
